a161d79c50e562c4a905fc73700b6a5385d59484
Alberto <albicento.ac@gmail.com>
2026-01-13 16:49:07 +0100

    chore: stricter ctor tests

--- tests/unitary/ema/conftest.py
+++ tests/unitary/ema/conftest.py
@@ -6,7 +6,10 @@ from tests.utils.deployers import EMA_DEPLOYER
 
 DUMMY_ALLOWED_EMAS = ["ema0", "ema1", "ema2", "ema3", "ema4"]
 # EMAConfig struct: (ema_id, initial_value, ema_time)
-DUMMY_EMA_CONFIGS = [(ema_id, 10**18, 600) for ema_id in DUMMY_ALLOWED_EMAS]
+DUMMY_EMA_CONFIGS = [
+    (ema_id, (i + 1) * 10**18, (i + 1) * 600)
+    for i, ema_id in enumerate(DUMMY_ALLOWED_EMAS)
+]
 
 
 @pytest.fixture
--- tests/unitary/ema/test_ctor_ema.py
+++ tests/unitary/ema/test_ctor_ema.py
@@ -7,7 +7,12 @@ from tests.utils.deployers import EMA_DEPLOYER
 def test_default_behavior():
     """Constructor should persist the allowed EMA ids as provided."""
     for size in range(MAX_EMAS + 1):
-        allowed = [f"e{index:03d}" for index in range(size)]
-        ema = EMA_DEPLOYER.deploy(allowed)
+        allowed_ids = [f"e{index:03d}" for index in range(size)]
+        # EMAConfig struct: (ema_id, initial_value, ema_time)
+        configs = [
+            (ema_id, (i + 1) * 10**18, (i + 1) * 600)
+            for i, ema_id in enumerate(allowed_ids)
+        ]
+        ema = EMA_DEPLOYER.deploy(configs)
         observed = [ema.ALLOWED_EMAS(i) for i in range(size)]
-        assert observed == allowed
+        assert observed == allowed_ids
