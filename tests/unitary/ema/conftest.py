74e27482ce2e4608d53b684a0aa664e191a3edfa
Alberto <albicento.ac@gmail.com>
2026-01-13 14:06:47 +0100

    test: fix conftest ctor

--- tests/unitary/ema/conftest.py
+++ tests/unitary/ema/conftest.py
@@ -5,9 +5,11 @@ import pytest
 from tests.utils.deployers import EMA_DEPLOYER
 
 DUMMY_ALLOWED_EMAS = ["ema0", "ema1", "ema2", "ema3", "ema4"]
+# EMAConfig struct: (ema_id, initial_value, ema_time)
+DUMMY_EMA_CONFIGS = [(ema_id, 10**18, 600) for ema_id in DUMMY_ALLOWED_EMAS]
 
 
 @pytest.fixture
 def ema():
     """Deploy an EMA contract seeded with predictable ids."""
-    return EMA_DEPLOYER.deploy(DUMMY_ALLOWED_EMAS)
+    return EMA_DEPLOYER.deploy(DUMMY_EMA_CONFIGS)
